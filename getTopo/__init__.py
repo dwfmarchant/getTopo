dd81fa260c56dba16766f5295dbf6f2e8d829e40
Dave Marchant <dwfmarchant@gmail.com>
2017-01-10 11:50:17 -0800

    Added downloadSRTM_LL function

--- getTopo/__init__.py
+++ getTopo/__init__.py
@@ -49,6 +49,7 @@ def downloadSRTMtile(lat, lon):
         urllib.request.HTTPCookieProcessor(cookie_jar))
     urllib.request.install_opener(opener)
 
+    print('\tDownloading {}....'.format(fname), end="", flush=True)
     request = urllib.request.Request(URL+fname)
     response = urllib.request.urlopen(request)    
 
@@ -63,5 +64,41 @@ def downloadSRTMtile(lat, lon):
     srtm = np.fromstring(string=hgt_string, dtype='int16').byteswap()
     srtm.shape = (3601, 3601)
     srtm = np.flipud(srtm)
+    print('Done', flush=True)
 
     return srtm
+
+def downloadSRTM_LL(lat_min, lat_max, lon_min, lon_max):
+
+    lons = np.r_[int(np.floor(lon_min)):int(np.floor(lon_max)+1)]
+    lats = np.r_[int(np.floor(lat_min)):int(np.floor(lat_max).max()+1)]
+    tiles_lon, tiles_lat = np.meshgrid(lons, lats)
+
+    hgt_dict = {}
+    print('Need {} tiles...'.format(tiles_lon.size))
+    sys.stdout.flush()
+    for lon, lat in zip(tiles_lon.flat, tiles_lat.flat):
+        hgt_dict[(lon, lat)] = downloadSRTMtile(lat, lon)
+
+    hgt = np.ndarray((0, lons.size*3600+1))
+    for ilat in lats:
+        hgt_r = np.ndarray((3600+1, 0))
+        for ilon in lons:
+            hgt_r = np.hstack([hgt_r, hgt_dict[(ilon, ilat)][:, :-1]])
+        hgt_r = np.hstack([hgt_r, hgt_dict[(lons[-1], ilat)][np.newaxis, -1].T])
+        hgt = np.vstack([hgt, hgt_r[:-1, :]])
+    hgt = np.vstack([hgt, hgt_r[-1, :]])
+
+    x_ll = np.linspace(lons.min(), lons.max()+1, lons.size*3600 + 1)
+    y_ll = np.linspace(lats.min(), lats.max()+1, lats.size*3600 + 1)
+
+    lon_ind0 = np.where(x_ll >= lon_min)[0][0]
+    lon_ind1 = np.where(x_ll <= lon_max)[0][-1]
+    lat_ind0 = np.where(y_ll >= lat_min)[0][0]
+    lat_ind1 = np.where(y_ll <= lat_max)[0][-1]
+
+    y_ll_trim = y_ll[lat_ind0:lat_ind1+1]
+    x_ll_trim = x_ll[lon_ind0:lon_ind1+1]
+    hgt_trim = hgt[lat_ind0:lat_ind1+1, lon_ind0:lon_ind1+1]
+
+    return x_ll_trim, y_ll_trim, hgt_trim
