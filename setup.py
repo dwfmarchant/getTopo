96a15b6bd75fd19d8a953cfa841f338c026d142e
ben <ben@compgeoinc.com>
2020-03-26 15:48:13 -0700

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="getTopo",
+    version="0.0.0",
+    description="Download SRTM Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pyproj'.
+        'json',
+        'os',
+        'urllib',
+        'http',
+        'io',
+        'zipfile',
+        'sys'
+
+        
+    ],
+)
