44b7eada7bcdaa2f549bff880d468d040f0268dc
ben <ben@compgeoinc.com>
2020-09-18 10:39:21 -0700

    Prepare for release v0.1.0 - Beta1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="getTopo",
-    version="0.0.0",
+    version="0.1.0",
     description="Download SRTM Data",
     long_description=long_description,
     long_description_content_type="text/markdown",
